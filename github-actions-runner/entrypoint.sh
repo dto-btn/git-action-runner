0173bc5d41177811cf6deff00ec40344ba09c966
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-10-07 15:52:46 -0400

    change to env variables.  combine jwt into entry

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,6 +1,44 @@
 #!/bin/sh -l
 
-JWTTOKEN=$(./jwt.sh $CLIENT_ID "./$KEY_FILE_NAME.pem")
+set -o pipefail
+# CLIENT_ID=$1 # Client ID as first argument
+
+# pem=$( cat $2 ) # file path of the private key as second argument.  comment this out and use ${PEM} to bring in env var from terraform
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issues 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+# Header encode
+header=$( echo -n "${header_json}" | b64enc )
+
+payload_json="{
+    \"iat\":${iat},
+    \"exp\":${exp},
+    \"iss\":\"${CLIENT_ID}\"
+}"
+# Payload encode
+payload=$( echo -n "${payload_json}" | b64enc )
+
+# Signature
+header_payload="${header}"."${payload}"
+# ${pem} here to ${PEM} after bring in from env var in terraform
+signature=$(
+    openssl dgst -sha256 -sign <(echo -n "${PEM}") \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+JWTTOKEN="${header_payload}"."${signature}"
+
+
+# JWTTOKEN=$(./jwt.sh $CLIENT_ID "./$KEY_FILE_NAME.pem")
 echo "12312: $JWTTOKEN"
 
 # get an access token
