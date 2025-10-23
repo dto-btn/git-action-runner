f852cf8e6517f66321052fc0f3a70e0a43524471
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-10-23 13:11:48 -0400

    debug messages

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -34,6 +34,7 @@ signature=$(
 
 # Create JWT
 JWTTOKEN="${header_payload}"."${signature}"
+echo "12312: $JWTTOKEN"
 
 # get an access token
 ACCESS_TOKEN="$(curl --request POST -fsSL --http1.1 \
@@ -43,6 +44,8 @@ ACCESS_TOKEN="$(curl --request POST -fsSL --http1.1 \
   "https://api.github.com/app/installations/$INSTALLATION_ID/access_tokens" \
   | jq -r '.token')"
 
+  echo "access: $ACCESS_TOKEN"
+
 # Retrieve a short lived runner registration token using the access token
 REGISTRATION_TOKEN="$(curl -X POST -fsSL --http1.1 \
   -H 'Accept: application/vnd.github.v3+json' \
@@ -51,4 +54,5 @@ REGISTRATION_TOKEN="$(curl -X POST -fsSL --http1.1 \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
 
+echo "reg: $REGISTRATION_TOKEN"
 ./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
