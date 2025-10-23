a428766e94206fce318f3173790d08604a7c17c6
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-10-23 10:07:50 -0400

    cleanup comments

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -o pipefail
-# CLIENT_ID=$1 # Client ID as first argument
-
-# pem=$( cat $2 ) # file path of the private key as second argument.  comment this out and use ${PEM} to bring in env var from terraform
 
 now=$(date +%s)
 iat=$((${now} - 60)) # Issues 60 seconds in the past
@@ -15,6 +12,7 @@ header_json='{
     "typ":"JWT",
     "alg":"RS256"
 }'
+
 # Header encode
 header=$( echo -n "${header_json}" | b64enc )
 
@@ -28,7 +26,7 @@ payload=$( echo -n "${payload_json}" | b64enc )
 
 # Signature
 header_payload="${header}"."${payload}"
-# ${pem} here to ${PEM} after bring in from env var in terraform
+
 signature=$(
     openssl dgst -sha256 -sign <(echo -n "${PEM}") \
     <(echo -n "${header_payload}") | b64enc
@@ -36,10 +34,6 @@ signature=$(
 
 # Create JWT
 JWTTOKEN="${header_payload}"."${signature}"
-
-
-# JWTTOKEN=$(./jwt.sh $CLIENT_ID "./$KEY_FILE_NAME.pem")
-echo "12312: $JWTTOKEN"
 
 # get an access token
 ACCESS_TOKEN="$(curl --request POST -fsSL --http1.1 \
@@ -49,16 +43,12 @@ ACCESS_TOKEN="$(curl --request POST -fsSL --http1.1 \
   "https://api.github.com/app/installations/$INSTALLATION_ID/access_tokens" \
   | jq -r '.token')"
 
-echo "access: $ACCESS_TOKEN"
-
-# Retrieve a short lived runner registration token using the PAT
+# Retrieve a short lived runner registration token using the access token
 REGISTRATION_TOKEN="$(curl -X POST -fsSL --http1.1 \
   -H 'Accept: application/vnd.github.v3+json' \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
-
-echo "reg: $REGISTRATION_TOKEN"
 
 ./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
